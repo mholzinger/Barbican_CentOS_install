99f11e4e06549cd8f7a3d06dcb0e68c52d059688
Mike Holzinger <mike.holzinger@gmail.com>
2014-04-16 17:10:34 -0700

    Clone, install and run Barbican key manager

---
+++ clone_install_run_barbican.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Work from HOME today
+cd ~
+
+# Clone barbican to HOME
+git clone https://github.com/stackforge/barbican.git
+
+# Start working
+cd barbican
+bin/barbican.sh install
+
+
